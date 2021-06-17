b1fb213551aae7753303aef9d04d362f2f132f8d
Rushi-Bhavsar <rushi.bhavsar.57@gmail.com>
2021-06-17 10:29:03 +0530

    DRF get method api completed.

---
+++ first_drf_project/fake_data_script.py
@@ -0,0 +1,22 @@
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_drf_project.settings')
+import django
+
+django.setup()
+from first_drf_get_api.models import StudentModel
+from faker import Faker
+
+
+def populate(n=5):
+    fake = Faker()
+    for entry in range(n):
+        fake_name = fake.name()
+        fake_roll_number = fake.random_int(min=1, max=100, step=1)
+        fake_city = fake.city()
+        student_details_object = StudentModel.objects.get_or_create(name=fake_name, roll_no=fake_roll_number,
+                                                                    city=fake_city)[0]
+        student_details_object.save()
+
+
+populate(20)
--- first_drf_project/first_drf_get_api/views.py
+++ first_drf_project/first_drf_get_api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import StudentModel
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+
+# Model Object - Single Student Data.
+def student_info_selected(request, roll_no):
+    # Query set, Complex data type.
+    student_model_object = StudentModel.objects.get(id=roll_no)
+    # Native Python Data type.
+    student_serializer_object = StudentSerializer(student_model_object)
+    # json_data = JSONRenderer().render(student_serializer_object.data)
+    # return HttpResponse(json_data, content_type='application/json')
+    return JsonResponse(student_serializer_object.data)
+
+
+def student_info_selected_by_roll_no(request):
+    student_model_object = StudentModel.objects.all()
+    student_serializer_object = StudentSerializer(student_model_object, many=True)
+    # json_data = JSONRenderer().render(student_serializer_object.data)
+    # return HttpResponse(json_data, content_type='application/json')
+    return JsonResponse(student_serializer_object.data, safe=False)
